2291991b45ba28f4aed2d1b3103cdde53fa2bb54
Taniya <tanya.kalra200@gmail.com>
2023-07-11 22:20:27 +0530

    created

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [val,setval] = useState("");
+
+  function print(e){
+    return setval(e.target.value);
+  }
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <label>Enter your name:</label> <br/>
+        <input type="text" value={val} onChange={print} />
+        <p>{"Hello " + val}</p>
     </div>
   )
 }
